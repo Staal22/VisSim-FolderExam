57fbfd607f3ed1bced966148c1950c42cf41522c
Stål Brændeland <staalbr@gmail.com>
2023-10-18 15:44:30 +0200

    GPU Instancing
    Now we can draw all ~five million points with liveable performance.

--- Assets/Code/TerrainManager.cs
+++ Assets/Code/TerrainManager.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Linq;
 using System.IO;
 using UnityEngine;
 
 public class TerrainManager : MonoBehaviour
 {
     public static TerrainManager Instance;
-    [SerializeField] private GameObject pointPrefab;
+    // [SerializeField] private GameObject pointPrefab;
+    [SerializeField] private Mesh instanceMesh;
+    [SerializeField] private Material instanceMaterial;
+    private const int BufferSize = 1023; // must not exceed 1023
+    private Matrix4x4[][] _batches;
     private const string OutputFilePath = "Assets/StreamingAssets/terrain.txt";
     
     private void Awake()
@@ -15,42 +20,62 @@ public class TerrainManager : MonoBehaviour
 
     private void Start()
     {
-        DrawPointCloud();
+        InitializePointData();
     }
     
-    private void DrawPointCloud()
+    private void Update()
     {
-        try
+        // draw all batches
+        for (int batchIndex = 0; batchIndex < _batches.Length; ++batchIndex)
         {
-            string[] lines = File.ReadAllLines(OutputFilePath);
-            
-            if (lines.Length == 0)
-            {
-                Debug.LogError("Terrain file is empty!");
-                return;
-            }
+            Graphics.DrawMeshInstanced(instanceMesh, 0, instanceMaterial, _batches[batchIndex], _batches[batchIndex].Length);
+        }
+    }
+    
+    private void InitializePointData()
+    {
+        var positions = LoadPositionsFromFile(OutputFilePath);
 
-            // Assuming the first line in the file contains the count of points, skipping it
-            for (int i = 1; i < lines.Length; i += 50) // draw 100k points instead of 5M
+        //Buffer size
+        int bufferSize = 1023;
+    
+        // Calculate the number of batches we'll need to render all instances
+        int batchCount = Mathf.CeilToInt((float)positions.Length / bufferSize);
+    
+        // allocate array for all batches
+        _batches = new Matrix4x4[batchCount][];
+
+        // for each batch...
+        for (int batchIndex = 0; batchIndex < batchCount; ++batchIndex)
+        {
+            // calculate the number of instances to render in this batch
+            // (for the last batch this might be less than bufferSize!)
+            int instanceCount = Mathf.Min(bufferSize, positions.Length - batchIndex * bufferSize);
+            _batches[batchIndex] = new Matrix4x4[instanceCount];
+
+            // prepare instance data for this batch
+            for (int i = 0; i < instanceCount; ++i)
             {
-                var line = lines[i];
-                var values = line.Split(' ');
-                if (values.Length < 3)
-                {
-                    Debug.LogError("Invalid line in terrain file: " + line);
-                    continue;
-                }
-                var xPos = float.Parse(values[0]);
-                var yPos = float.Parse(values[1]);
-                var zPos = float.Parse(values[2]);
-                
-                var point = Instantiate(pointPrefab);
-                point.transform.position = new Vector3(xPos - 260000, zPos, yPos - 6660000f);
+                // calculate the index into our positions array
+                int positionIndex = batchIndex * bufferSize + i;
+
+                // convert position to unity coords
+                var pos = positions[positionIndex];
+                var unityPos = new Vector3(pos.x - 260000, pos.z, pos.y - 6660000f);
+
+                // create transformation matrix
+                _batches[batchIndex][i] = Matrix4x4.TRS(unityPos, Quaternion.identity, Vector3.one);
             }
         }
-        catch (Exception e)
+    }
+    
+    private Vector3[] LoadPositionsFromFile(string path)
+    {
+        string[] lines = File.ReadAllLines(path).Skip(1).ToArray();
+        return lines.Select(line =>
         {
-            Debug.LogError("Failed to draw point cloud: " + e);
-        }
+            var values = line.Split(' ');
+            return new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]));
+        }).ToArray();
     }
 }
